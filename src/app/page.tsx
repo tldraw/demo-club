b900bad18fb4a4750b53a3942dfd49c48a7c87ee
Steve Ruiz <steveruizok@gmail.com>
2024-02-23 18:48:15 +0000

    ok

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<main>
+			<div className="hello">
+				<h1>demo club</h1>
+				<p>
+					by <a href="https://tldraw.com">tldraw</a>
+				</p>
+			</div>
+		</main>
+	)
+}
