63453df210b136a5350b726272078a18fcbc5c55
Steve Ruiz <steveruizok@gmail.com>
2024-05-17 10:41:01 -0700

    add links, people

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+import PEOPLE from "./PEOPLE.json"
+
 export default function Home() {
 	return (
 		<main>
 			<div className="hello">
-				<h1>demo club</h1>
-				<p>
-					by <a href="https://tldraw.com">tldraw</a>
-				</p>
+				<Image alt="demo club image" src="/demo-club.jpeg" width={600} height={600}/>
+				<ul>
+					{PEOPLE.map(person => (
+						<li key={person.name}><a href="https://x.com/steveruizok" target="_blank">{person.name}</a></li>
+					))}
+				</ul>
 			</div>
 		</main>
 	)
