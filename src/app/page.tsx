47432a9bcc863bfbf46488581155ff702a9896b8
Steve Ruiz <steveruizok@gmail.com>
2024-05-17 10:46:30 -0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
 	return (
 		<main>
 			<div className="hello">
-				<Image alt="demo club image" src="/demo-club.jpeg" width={600} height={600}/>
+				<Image alt="demo club image" src="/demo-club.jpeg" width={375} height={375}/>
 				<ul>
 					{PEOPLE.map(person => (
-						<li key={person.name}><a href="https://x.com/steveruizok" target="_blank">{person.name}</a></li>
+						<li key={person.name} ><a href={`https://x.com/${person.twitter}`} title={person.name}>{person.name}</a></li>
 					))}
 				</ul>
 			</div>
